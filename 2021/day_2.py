65945399e8e379895811db54a9c077d249bbb698
Sigmund K <sigmundklaa@outlook.com>
2021-12-02 19:31:29 +0000

    vroom vroom

--- 2021/day_2.py
+++ 2021/day_2.py
@@ -36,4 +36,19 @@ def puzzle_3(day, *args, **kwargs):
 def puzzle_4(day, *args, **kwargs):
     """
     Optimizations for puzzle 2 after having looked at other peoples solutions
+
+    Both puzzles can really be done in n iterations, with depth from 1 being the same as aim in 2,
+    making this the fastest solution
     """
+    inp = get_input(day)
+
+    hoz, depth, aim = 0, 0, 0
+
+    for k, v in inp:
+        if k == 'forward':
+            hoz += v
+            depth += aim * v
+        else:
+            aim += v if k == 'down' else -v
+
+    return hoz * depth, hoz * aim # 0 is puzzle 1, 1 is puzzle 2
