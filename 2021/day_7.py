b0fb4dc026c3cba82f5a8a0b49b180357d38c1a7
SigJig <sigmundklaa@outlook.com>
2021-12-07 20:53:20 +0100

    pretty but still shit :D

--- 2021/day_7.py
+++ 2021/day_7.py
@@ -3,33 +3,28 @@ https://adventofcode.com/2021/day/7
 """
 
 from utils import *
+from collections import deque
 
 def get_input(day):
     return [int(x) for x in day.input.read().split(',')]
 
+def gauss(start, stop):
+    """
+    https://stackoverflow.com/questions/20455977/sum-up-all-the-integers-in-range
+    """
+    return (stop - start) * (start + stop - 1) // 2
+
 @pass_input(get_input)
 def puzzle_1(inp, *args, **kwargs):
-    res = []
-    for iidx, i in enumerate(inp):
-        res.append(0)
-        for jidx, j in enumerate(inp):
-            if iidx == jidx:
-                continue
+    possibles = list(range(min(inp), max(inp) + 1))
 
-            res[iidx] += abs(j - i)
-
-    return min(res)
+    return min(sum(abs(x - y) for y in inp) for x in possibles)
 
 @pass_input(get_input)
 def puzzle_2(inp, *args, **kwargs):
-    res = []
-    mn, mx = min(inp), max(inp)
-    for iidx, i in enumerate(range(mn,mx+1)):
-        res.append(0)
-        for jidx, j in enumerate(inp):            
-            res[iidx] += sum(range(abs(j - i) + 1))
+    possibles = list(range(min(inp), max(inp) + 1))
 
-    return min(res)
+    return min(sum(gauss(0, abs(x - y) + 1) for y in inp) for x in possibles)
 
 @pass_input(get_input)
 def puzzle_3(inp, *args, **kwargs):
